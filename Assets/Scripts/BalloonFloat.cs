efd4cae6342c3181b19c57cfe61a2774dfe3a8c8
Ailatmemze <123972736+Ailatmemze@users.noreply.github.com>
2024-10-14 13:59:15 -0400

    New Map

---
+++ Assets/Scripts/BalloonFloat.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonFloat : MonoBehaviour
+{
+    public float upfloat = 0.5f; // Strength of the floating effect
+    public float floatSpeed = 1.0f; // Speed of the up and down motion
+
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position; // Save the initial position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the new y position based on a sine wave for oscillation
+        float newY = originalPosition.y + Mathf.Sin(Time.time * floatSpeed) * upfloat;
+        transform.position = new Vector3(originalPosition.x, newY, originalPosition.z);
+    }
+}
