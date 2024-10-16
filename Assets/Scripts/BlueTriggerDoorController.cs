e63148b57c301ce3d50d917d3a98ee6adad53045
Dank-Turtle-Man <jeff132015@yahoo.com>
2024-10-15 21:59:15 -0400

    new door assets and blue door script added

---
+++ Assets/Scripts/BlueTriggerDoorController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueTriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool openTrigger = false;
+
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("BlueKeyCard")){
+            if(openTrigger){
+                myDoor.Play("Door open", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
